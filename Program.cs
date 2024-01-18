b6dc2337df7c05aedd2523c4143f1c1c267da3d4
Timur <sirakanyantimur1@gmail.com>
2024-01-18 21:53:26 +0400

    Task2

--- Program.cs
+++ Program.cs
@@ -10,8 +10,64 @@
 // 4 3 4 1
 // 2 9 25 4   
 
-int [,] array = new int[3,4];
+// int [,] array = new int[3,4];
 
+
+// void GetArray()
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j <  array.GetLength(1); j++)
+//         {
+//         array[i,j]= new Random().Next(0,10);
+
+//         }
+//     }
+// }
+
+// void PrintArray()
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j <  array.GetLength(1); j++)
+//         {
+//             System.Console.Write(array[i,j]+ " ");
+//         }
+//         System.Console.WriteLine();
+//     }
+// }
+
+// void ChangeArray()
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j <  array.GetLength(1); j++)
+//         {
+//             if(i%2==0 && j%2 ==0)    
+//             {
+//                 array[i,j] *= array[i,j];
+//             }
+//         }
+//     }
+// }
+
+
+// GetArray();
+// PrintArray();
+// ChangeArray();
+// System.Console.WriteLine();
+// PrintArray();
+
+
+// Задайте двумерный массив. Найдите сумму элементов,
+// находящихся на главной диагонали (с индексами (0,0); (1;1) и
+// т.д.
+// Пример
+// 2 3 4 3
+//  4 3 4 1 => 2 + 3 + 5 = 10
+//  2 9 5 4
+
+int [,] array = new int[3,4];
 
 void GetArray()
 {
@@ -37,23 +93,24 @@ void PrintArray()
     }
 }
 
-void ChangeArray()
+int GetSum()
 {
+    int sum= 0;
+
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j <  array.GetLength(1); j++)
         {
-            if(i%2==0 && j%2 ==0)    
+            if(i==j)    
             {
-                array[i,j] *= array[i,j];
+                sum += array[i,j];
             }
         }
     }
+    return sum;
 }
-
 
 GetArray();
 PrintArray();
-ChangeArray();
-System.Console.WriteLine();
-PrintArray();
+
+System.Console.WriteLine(GetSum());
