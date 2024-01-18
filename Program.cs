49ba23fa038c87ad2c0d52539504b516b0ed97fc
Timur <sirakanyantimur1@gmail.com>
2024-01-18 21:31:49 +0400

    Task1

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿// Задайте двумерный массив. Найдите элементы, у которых оба
+// индекса чётные, и замените эти элементы на их квадраты.
+// Пример
+// 2 3 4 3
+//  4 3 4 1 =>
+//  2 9 5 4
+// Обсудить этапы решения задачи
+// Оформить этапы в виде функций
+// 4 3 4 3
+// 4 3 4 1
+// 2 9 25 4   
+
+int [,] array = new int[3,4];
+
+
+void GetArray()
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j <  array.GetLength(1); j++)
+        {
+        array[i,j]= new Random().Next(0,10);
+
+        }
+    }
+}
+
+void PrintArray()
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j <  array.GetLength(1); j++)
+        {
+            System.Console.Write(array[i,j]+ " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void ChangeArray()
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j <  array.GetLength(1); j++)
+        {
+            if(i%2==0 && j%2 ==0)    
+            {
+                array[i,j] *= array[i,j];
+            }
+        }
+    }
+}
+
+
+GetArray();
+PrintArray();
+ChangeArray();
+System.Console.WriteLine();
+PrintArray();
